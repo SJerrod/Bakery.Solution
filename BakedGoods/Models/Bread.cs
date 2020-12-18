454f8ba7e3c1ec1a4b06b2b029d0cb0c4bd3e486
Jerrod Styrk <jstyrk@citadel.edu>
2020-12-17 23:12:58 -0800

    Add test for bread cost and bread cost with buy 2 get 1 deal

--- BakedGoods/Models/Bread.cs
+++ BakedGoods/Models/Bread.cs
@@ -41,44 +41,4 @@ namespace BakedGoods.Models
       return _breadPrice;
     }
   }
-
-  public class Pastry
-  {
-    public float PastryCount { get; set; }
-    private float _pastryPrice;
-
-    public Pastry(float pastryCount, float pastryPrice)
-    {
-      PastryCount = pastryCount;
-      _pastryPrice = pastryPrice;
-    }
-
-    public float GetPastryPrice()
-    {
-      float treats = PastryCount;
-      _pastryPrice = 0;
-
-      if (treats == 0)
-      {
-        Console.WriteLine("You Currently have no Pastries for your order.");
-      }
-      else
-      {
-        while(treats > 0)
-        {
-          if (treats >= 3)
-          {
-            treats -= 3;
-            _pastryPrice += 5;
-          }
-          else if (treats < 3)
-          {
-            treats -= 1;
-            _pastryPrice += 2;
-          }
-        }
-      }
-      return _pastryPrice;
-    }
-  }
 }
---
+++ BakedGoods/Models/Pastry.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace BakedGoods.Models
+{
+  public class Pastry
+  {
+    public float PastryCount { get; set; }
+    private float _pastryPrice;
+
+    public Pastry(float pastryCount, float pastryPrice)
+    {
+      PastryCount = pastryCount;
+      _pastryPrice = pastryPrice;
+    }
+
+    public float GetPastryPrice()
+    {
+      float treats = PastryCount;
+      _pastryPrice = 0;
+
+      if (treats == 0)
+      {
+        Console.WriteLine("You Currently have no Pastries for your order.");
+      }
+      else
+      {
+        while(treats > 0)
+        {
+          if (treats >= 3)
+          {
+            treats -= 3;
+            _pastryPrice += 5;
+          }
+          else if (treats < 3)
+          {
+            treats -= 1;
+            _pastryPrice += 2;
+          }
+        }
+      }
+      return _pastryPrice;
+    }
+  }
+}
